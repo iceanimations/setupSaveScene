da7f641875feccc27fe77d56696253aa1e446998
Qurban <qurban_ali36@yahoo.com>
2015-02-18 12:33:31 +0500

    versioning based saveScene method implemented

--- src/_setupSave.py
+++ src/_setupSave.py
@@ -5,9 +5,16 @@ Created on Feb 10, 2015
 '''
 from loader.command.python import RedshiftAOVTools
 import pymel.core as pc
+import os.path as osp
+import os
+import uiContainer
+from PyQt4.QtGui import QMessageBox
 import appUsageApp
 import fillinout
+import msgBox
+import qtify_maya_window as qtfy
 import re
+import maya.cmds as cmds
 
 __title__ = 'Setup & Save Scene'
 __ep_regex__ = 'EP\d{2}'
@@ -54,6 +61,33 @@ def camExists(match):
             return cam
     return False
 
+def fileExists(path, fileName):
+    return fileName in [osp.splitext(name)[0] for name in os.listdir(path)]
+    
+def saveScene(path, fileName):
+    if fileExists(path, fileName):
+        versions = []
+        for version in os.listdir(path):
+            try:
+                versions.append(int(re.search('_v\d{3}', version).group().split('v')[-1]))
+            except AttributeError:
+                pass
+        if not versions:
+            fileName += '_v001'
+        else:
+            fileName = fileName +'_v'+ str(max(versions)+1).zfill(3)
+        btn = msgBox.showMessage(qtfy.getMayaWindow(), title=__title__,
+                                 msg='File already exists, your file will be saved as %s'%fileName,
+                                 ques='Do you want to proceed?',
+                                 icon=QMessageBox.Question,
+                                 btns=QMessageBox.Yes|QMessageBox.No)
+        if btn == QMessageBox.No:
+            return
+    fullPath = osp.join(path, fileName)
+    cmds.file(rename=fullPath)
+    cmds.file(f=True, save=True, options="v=0;", type=cmds.file(q=True, type=True)[0])
+
+
 def setupScene():
     if pc.mel.currentRenderer().lower() != 'redshift':
         pc.mel.setCurrentRenderer('redshift')
@@ -95,11 +129,21 @@ def setupScene():
     
     setResolution()
     
-    pc.inViewMessage(amg='<hl>Scene setup successful</hl>', pos='midCenter', fade=True )
-    
+    #pc.inViewMessage(amg='<hl>Scene setup successful</hl>', pos='midCenter', fade=True )
+    path = osp.join(r'D:\shot_test', ep, 'SEQUENCES', seq, 'SHOTS', '_'.join([seq, sh]), 'lighting', 'files')
+    #path = osp.join(r'P:\external\Al_Mansour_Season_02\02_production', ep, 'SEQUENCES', seq, 'SHOTS', '_'.join([seq, sh]), 'lighting', 'files')
+    if not osp.exists(path):
+        msgBox.showMessage(qtfy.getMayaWindow(), title=__title__,
+                           msg='Unable to save the file because the system could not find the constructed path\n'+path,
+                           icon=QMessageBox.Information)
+        return
+    btn = msgBox.showMessage(qtfy.getMayaWindow(), title=__title__,
+                             msg='Your scene will be save at\n'+
+                             path, icon=QMessageBox.Question,
+                             ques='Is this location correct?',
+                             btns=QMessageBox.Yes|QMessageBox.No)
+    if btn == QMessageBox.No:
+        return
+    saveScene(path, '_'.join([ep, seq, sh]))
+        
     appUsageApp.updateDatabase('setupSaveScene')
-    
-    
-
-def saveScene():
-    pass
